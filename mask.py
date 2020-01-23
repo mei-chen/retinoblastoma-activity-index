301c3e7664911beea82665be2c001863c01d71cc
Mei-lin-chen <mei.lin.chen@hotmail.com>
2020-01-22 16:17:17 -0800

    mask for anterior compartment created

--- mask.py
+++ mask.py
@@ -1,7 +1,11 @@
+import os
 from PIL import Image, ImageOps, ImageDraw
-
+#open up image
 image = Image.open("background.png")
+
+#draw centre circle
 draw = ImageDraw.Draw(image)
+draw_bg = ImageDraw.Draw(image)
 x = 391
 y = 391
 r = 321
@@ -9,8 +13,12 @@ leftUpPoint = (x-r, y-r)
 rightDownPoint = (x+r, y+r)
 twoPointList = [leftUpPoint, rightDownPoint]
 draw.ellipse(twoPointList, fill='black')
+#draw.rectangle(twoPointList, fill = 'white')
 image.show()
+
 
 back = Image.new('RGBA', image.size)
 back.paste(image, mask = 200 )
-back.show()
+
+#make inverse mask by first filling in entire image black 
+draw_bg.rectangle()
---
+++ mask_alpha.py
@@ -0,0 +1,25 @@
+from PIL import Image, ImageOps, ImageDraw
+
+#crop out the edge around the circle --> mask 
+size = (781, 781)
+mask = Image.new('L', size, 0)
+draw = ImageDraw.Draw(mask) 
+draw.ellipse((0, 0) + size, fill=255)
+
+
+#draw the centre of the circle to include only the anterior 
+x, y, r = 391, 391, 321
+leftUpPoint = (x-r, y-r)
+rightDownPoint = (x+r, y+r)
+twoPointList = [leftUpPoint, rightDownPoint]
+draw.ellipse(twoPointList, fill='black')
+ 
+# mask.show()
+
+#turn the white pixels transparent 
+
+#crop
+bg_image = Image.open("background.png")
+bg_image.putalpha(mask)
+bg_image_crop = bg_image.crop((0, 0, 781, 781))
+bg_image_crop.show()
