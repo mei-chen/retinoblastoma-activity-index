ef1bfef2fd2533f1520226a10eaccb36daf7b7ec
Mei-lin-chen <mei.lin.chen@hotmail.com>
2020-01-27 02:35:34 -0800

    fovea mask

--- fovea_mask.py
+++ fovea_mask.py
@@ -6,17 +6,15 @@ mask = Image.new('L', size, 0)
 draw = ImageDraw.Draw(mask) 
 draw.ellipse((0, 0) + size, fill=255)
 
-
-#draw the centre of the circle to include only the anterior 
-x, y, r = 391, 391, 321
+#draw the centre of the circle at where the fovea is (centre of the middle X) to indicate the points that we would pass 
+x, y, r = 390, 392, 2
 leftUpPoint = (x-r, y-r)
 rightDownPoint = (x+r, y+r)
 twoPointList = [leftUpPoint, rightDownPoint]
-draw.ellipse(twoPointList, fill='black')
- 
-# mask.show()
+draw.ellipse(twoPointList, fill='blue')
 
-#turn the white pixels transparent
+#Isolate the pixels above and check if they are yellow
+
 
 #crop
 bg_image = Image.open("background.png")
